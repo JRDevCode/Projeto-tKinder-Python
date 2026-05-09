adc6a9fe74a413b7ee7db986403944b1557501a0
João Ricardo <joaoricardo1127@gmail.com>
2026-05-08 22:49:00 -0300

    Projeto tKinder (Função Limpar-tela foi introduzida)

--- venv/Aula01.py
+++ venv/Aula01.py
@@ -5,7 +5,15 @@ from tkinter import ttk
 # Janela do sistema
 root = Tk()
 
-class Application():
+class Funcs():
+    def limpar_tela(self):
+        self.codigo_entry.delete(0, END)
+        self.nome_entry.delete(0, END)
+        self.fone_entry.delete(0, END)
+        self.cidade_entry.delete(0, END)
+
+
+class Application(Funcs):
     def __init__(self):
         self.root = root
         self.tela()
@@ -37,28 +45,28 @@ class Application():
     def widgets_frame1(self):
         ### Criação do botao limpar
         self.bt_limpar = Button(self.frame_1, text= 'Limpar', bd= 2, bg= "#2aa5ec", fg= "#FFFFFF"
-                                ,font= ('verdana', 8, 'bold'))
+                                ,font= ('verdana', 8, 'bold'), command= self.limpar_tela)
         self.bt_limpar.place(relx= 0.2, rely= 0.1, relwidth= 0.1, relheight= 0.15)
 
          ### Criação do botao buscar
-        self.bt_limpar = Button(self.frame_1, text= 'Buscar', bd= 3, bg= "#2aa5ec", fg= "#FFFFFF"
+        self.bt_buscar = Button(self.frame_1, text= 'Buscar', bd= 3, bg= "#2aa5ec", fg= "#FFFFFF"
                                 ,font= ('verdana', 8, 'bold'))
-        self.bt_limpar.place(relx= 0.3, rely= 0.1, relwidth= 0.1, relheight= 0.15)
+        self.bt_buscar.place(relx= 0.3, rely= 0.1, relwidth= 0.1, relheight= 0.15)
 
         ### Criação do botao novo
-        self.bt_limpar = Button(self.frame_1, text= 'Novo', bd= 2, bg= "#2aa5ec", fg= "#FFFFFF"
+        self.bt_novo = Button(self.frame_1, text= 'Novo', bd= 2, bg= "#2aa5ec", fg= "#FFFFFF"
                                 ,font= ('verdana', 8, 'bold'))
-        self.bt_limpar.place(relx= 0.6, rely= 0.1, relwidth= 0.1, relheight= 0.15)
+        self.bt_novo.place(relx= 0.6, rely= 0.1, relwidth= 0.1, relheight= 0.15)
 
         ### Criação do botao alterar
-        self.bt_limpar = Button(self.frame_1, text= 'Alterar', bd= 2, bg= "#2aa5ec", fg= "#FFFFFF"
+        self.bt_alterar = Button(self.frame_1, text= 'Alterar', bd= 2, bg= "#2aa5ec", fg= "#FFFFFF"
                                 ,font= ('verdana', 8, 'bold'))
-        self.bt_limpar.place(relx= 0.7, rely= 0.1, relwidth= 0.1, relheight= 0.15)
+        self.bt_alterar.place(relx= 0.7, rely= 0.1, relwidth= 0.1, relheight= 0.15)
 
         ### Criação do botao apagar
-        self.bt_limpar = Button(self.frame_1, text= 'Apagar', bd= 3, bg= "#2aa5ec", fg= "#FFFFFF"
+        self.bt_apagar = Button(self.frame_1, text= 'Apagar', bd= 3, bg= "#2aa5ec", fg= "#FFFFFF"
                                 ,font= ('verdana', 8, 'bold'))
-        self.bt_limpar.place(relx= 0.8, rely= 0.1, relwidth= 0.1, relheight= 0.15)
+        self.bt_apagar.place(relx= 0.8, rely= 0.1, relwidth= 0.1, relheight= 0.15)
 
         ## Criação da label e entrada do codigo
         self.lb_codigo = Label(self.frame_1, text = 'Código', bg= "#EDEDF7", fg= "#2aa5ec")
@@ -75,18 +83,18 @@ class Application():
         self.nome_entry.place(relx= 0.05, rely= 0.45, relwidth= 0.85)
 
         ## Criação da label e entrada do telefone
-        self.lb_codigo = Label(self.frame_1, text = 'Telefone', bg= "#EDEDF7", fg= "#2aa5ec")
-        self.lb_codigo.place(relx= 0.05, rely= 0.6 )
+        self.lb_fone = Label(self.frame_1, text = 'Telefone', bg= "#EDEDF7", fg= "#2aa5ec")
+        self.lb_fone.place(relx= 0.05, rely= 0.6 )
 
-        self.codigo_entry = Entry(self.frame_1, bg= "#EAF8FE")
-        self.codigo_entry.place(relx= 0.05, rely= 0.7, relwidth= 0.4)
+        self.fone_entry = Entry(self.frame_1, bg= "#EAF8FE")
+        self.fone_entry.place(relx= 0.05, rely= 0.7, relwidth= 0.4)
 
         ## Criação da label e entrada do cidade
-        self.lb_codigo = Label(self.frame_1, text = 'Cídade', bg= "#EDEDF7", fg= "#2aa5ec")
-        self.lb_codigo.place(relx= 0.5, rely= 0.6 )
+        self.lb_cidade = Label(self.frame_1, text = 'Cídade', bg= "#EDEDF7", fg= "#2aa5ec")
+        self.lb_cidade.place(relx= 0.5, rely= 0.6 )
 
-        self.codigo_entry = Entry(self.frame_1, bg= "#EAF8FE")
-        self.codigo_entry.place(relx= 0.5, rely= 0.7, relwidth= 0.4)
+        self.cidade_entry = Entry(self.frame_1, bg= "#EAF8FE")
+        self.cidade_entry.place(relx= 0.5, rely= 0.7, relwidth= 0.4)
 
     def lista_frame2(self):
         self.listaCli = ttk.Treeview(self.frame_2, height= 3, columns= ('col1', "col2", "col3", "col4"))
